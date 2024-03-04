cca35cdc81fc19b20ed060098ab12e7db83cad05
Yannick Funes <108564032+Yannx79@users.noreply.github.com>
2024-03-03 23:13:42 -0500

    Create docker-menu.sh

---
+++ menus/docker-menu.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+show() {
+    clear
+
+    echo "Main Menu"
+    echo "1. Show Images"
+    echo "2. Show Networks"
+    echo "3. Show Docker Processes"
+    echo "4. Show All Docker Processes" 
+    echo "5. Show Volumes"
+    echo "11. Exit"
+}
+
+get_input() {
+    read -p "Choose your choice: " option
+}
+
+show_images() {
+    docker images
+}
+
+show_networks() {
+    docker network ls
+}
+
+show_docker_processes() {
+    docker ps
+}
+
+show_all_docker_processes() {
+    docker ps -a
+}
+
+show_volumes() {
+    docker volume ls
+}
+
+while true; do
+    show
+    get_input
+
+    case $option in
+        1) show_images;;
+        2) show_networks;;
+        3) show_docker_processes;;
+        4) show_all_docker_processes;;
+        5) show_volumes;;
+        11) 
+            echo "Leaving the menu ..."
+            sleep 1
+            clear
+            exit;;
+        *)
+            echo "Invalid option";;
+    esac
+
+    read -p "Press any key to continues ..."
+
+done
+
+
+
